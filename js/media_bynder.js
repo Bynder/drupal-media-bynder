fc5881c8827fd06d9fe408d3a4a72cf1f6cd3821
Michiel Westerbeek <michiel@labela.nl>
2015-04-20 12:05:38 +0200

    add image directly if in article mode, else normal behaviour

--- js/media_bynder.js
+++ js/media_bynder.js
@@ -1,4 +1,6 @@
 (function ($) {
+    'use strict';
+
     function showAlert(message, type) {
         var alert = $('#edit-bynder-search .alert');
             alert.find('span.text').text(message);
@@ -9,6 +11,7 @@
             alert.fadeOut(1000);
         }, 2500);
     }
+
     $(document).ready(function(){
         $('#edit-bynder-search .normal_facet_list > .facet_title').click(function() {
             $(this).find('.expand i').toggleClass('fa-angle-down').toggleClass('fa-angle-up');
@@ -101,41 +104,51 @@
             var id = $image.attr('data-id');
             var idHash = $image.attr('data-idHash');
 
-            $.ajax({
-                url: '/?q=admin/content/media/add/media_bynder',
-                type: 'POST',
-                dataType: 'json',
-                data: {
-                    id: id,
-                    idHash: idHash
-                },
-                success: function(data) {
-                    $image.removeClass('loading');
-                    spinner.stop();
-                    showAlert(data.message, data.type);
-                }
-            });
+            var media_library_mode = $('body').hasClass('page-admin-content-media-add-media-bynder');
+
+            if(media_library_mode){
+                $.ajax({
+                    url: '/?q=admin/content/media/add/media_bynder',
+                    type: 'POST',
+                    dataType: 'json',
+                    data: {
+                        id: id,
+                        idHash: idHash
+                    },
+                    success: function(data) {
+                        $image.removeClass('loading');
+                        spinner.stop();
+                        showAlert(data.message, data.type);
+                    }
+                });
+            }else{
+                $('input[name="selected_asset"]').val('bynder://f/' + id + '/i/' + idHash);
+                $('#media-bynder-add').submit();
+            }
         });
 
         $('#media-bynder-add').submit(function() {
-            var spinner = new Spinner({
-                lines: 13,
-                length: 20,
-                width: 10,
-                radius: 30,
-                corners: 1,
-                rotate: 0,
-                direction: 1,
-                color: '#000',
-                speed: 1,
-                trail: 60,
-                shadow: false,
-                hwaccel: false,
-                className: 'spinner',
-                zIndex: 2e9,
-                top: '50%',
-                left: '50%'
-            }).spin($('#edit-bynder-search')[0]);
+            var media_library_mode = $('body').hasClass('page-admin-content-media-add-media-bynder');
+            if(media_library_mode){
+                var spinner = new Spinner({
+                    lines: 13,
+                    length: 20,
+                    width: 10,
+                    radius: 30,
+                    corners: 1,
+                    rotate: 0,
+                    direction: 1,
+                    color: '#000',
+                    speed: 1,
+                    trail: 60,
+                    shadow: false,
+                    hwaccel: false,
+                    className: 'spinner',
+                    zIndex: 2e9,
+                    top: '50%',
+                    left: '50%'
+                }).spin($('#edit-bynder-search')[0]);
+            }
         });
     });
 })(jQuery);

25eaec6c632923d287cba020c8934911e75cbd1a
Thomas Moors <thomas.moors@getbynder.com>
2015-12-10 17:14:51 +0100

    overlooked log

--- js/image.js
+++ js/image.js
@@ -1,7 +1,6 @@
 (function ($) {
 	$( document ).ready(function(){
 		$('img').one('error', function() {
-			console.log('yo ben er');
 			$(this).attr("src", "/sites/all/modules/drupal-media-bynder-develop/assets/bynder.png");
 		});
 	});

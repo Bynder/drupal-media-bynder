1266a1e87fc98c20dcfdcb2051a41aabfcb06a3e
Thomas Moors <thomas.moors@getbynder.com>
2015-12-11 14:42:18 +0100

    comments and jQuery object indication

--- js/image.js
+++ js/image.js
@@ -1,18 +1,22 @@
+/* Try to reload the image 5 times with a 1 second pause.
+Do this by replacing to image source.
+After 5 tries display the alt text to contact Bynder. */
+
 (function ($) {
 	var retries = 5;
 	$( document).ready(function(){
 		$('img').bind('error', function() {
-			var image = $(this);
-			var imgSrc = image.attr('src');
-			if (retries > 0 && typeof imgSrc !== undefined) {
+			var $image = $(this);
+			var $imgSrc = $image.attr('src');
+			if (retries > 0 && $imgSrc !== undefined) {
 				window.setTimeout(function(){
-					image.attr('src', imgSrc);
+					$image.attr('src', $imgSrc);
 					retries--;
-				},1000);
+				}, 1000);
 			} else {
-				image.unbind('error');
-				image.attr('title', 'The image could not be retrieved, please contact Bynder Support');
-				image.attr('alt', 'The image could not be retrieved, please contact Bynder Support');
+				$image.unbind('error');
+				$image.attr('title', 'The image could not be retrieved, please contact Bynder Support');
+				$image.attr('alt', 'The image could not be retrieved, please contact Bynder Support');
 			}
 		});
 	});

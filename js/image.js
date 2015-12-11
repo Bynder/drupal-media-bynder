9a60830b1faaaf4fe6c3bad01e3881d5168060bb
Thomas Moors <thomas.moors@getbynder.com>
2015-12-11 13:28:28 +0100

    retry for image

--- js/image.js
+++ js/image.js
@@ -1,7 +1,18 @@
 (function ($) {
-	$( document ).ready(function(){
-		$('img').one('error', function() {
-			$(this).attr("src", "/sites/all/modules/drupal-media-bynder-develop/assets/bynder.png");
+	var retries = 5;
+	$( document).ready(function(){
+		$('img').bind('error', function() {
+			var image = $(this);
+			var imgSrc = image.attr('src');
+			if (retries > 0 && typeof imgSrc !== undefined) {
+				window.setTimeout(function(){
+					image.attr('src', imgSrc);
+					retries--;
+				},1000);
+			} else {
+				image.unbind('error');
+				image.attr('alt', 'The image could not be retrieved, please contact Bynder Support');
+			}
 		});
 	});
 })(jQuery);

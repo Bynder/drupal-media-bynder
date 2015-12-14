913ae1121dd4d1e160e07541488c3c34656c3bae
Thomas Moors <thomas.moors@getbynder.com>
2015-12-14 09:52:40 +0100

    recursive script

--- js/image.js
+++ js/image.js
@@ -1,23 +1,37 @@
 /* Try to reload the image 5 times with a 1 second pause.
 Do this by replacing to image source.
-After 5 tries display the alt text to contact Bynder. */
+After 5 tries just display the alt text to contact Bynder. */
 
 (function ($) {
 	var retries = 5;
 	$( document).ready(function(){
-		$('img').bind('error', function() {
+		$('.field-items .field-item img').one('error', function() {
 			var $image = $(this);
-			var $imgSrc = $image.attr('src');
-			if (retries > 0 && $imgSrc !== undefined) {
-				window.setTimeout(function(){
-					$image.attr('src', $imgSrc);
-					retries--;
-				}, 1000);
-			} else {
-				$image.unbind('error');
-				$image.attr('title', 'The image could not be retrieved, please contact Bynder Support');
-				$image.attr('alt', 'The image could not be retrieved, please contact Bynder Support');
+			$image.attr('title', 'The image could not be retrieved, please contact Bynder Support');
+			$image.attr('alt', 'The image could not be retrieved, please contact Bynder Support');
+			if (typeof $image !== 'undefined') {
+				if (typeof $image.attr('src') !== 'undefined') {
+					$image.attr('src', retryToLoadImage($image));
+				}
 			}
 		});
 	});
+
+	function retryToLoadImage($img) {
+		var $newImg = $('<img>');
+		var $src = ($img.attr('src')) || '';
+		$newImg.attr('src', $src);
+		$newImg.one('error', function() {
+			window.setTimeout(function(){
+				if (retries > 0) {
+					retries--;
+					retryToLoadImage($newImg);
+				}
+			}, 1000);
+		});
+
+		$newImg.one('load', function() {
+			return $newImg.attr('src');
+		});
+	}
 })(jQuery);

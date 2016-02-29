ab055eef52ad2fa410fea85af605af38efab221a
Thomas Moors <thomas.moors@getbynder.com>
2016-02-29 16:54:31 +0100

    reload on the fly generated images automatical

--- js/image.js
+++ js/image.js
@@ -1,37 +1,10 @@
-/* Try to reload the image 5 times with a 1 second pause.
-Do this by replacing to image source.
-After 5 tries just display the alt text to contact Bynder. */
-
-(function ($) {
-	var retries = 5;
-	$( document).ready(function(){
-		$('.field-items .field-item img').one('error', function() {
-			var $image = $(this);
-			$image.attr('title', 'The image could not be retrieved, please contact Bynder Support');
-			$image.attr('alt', 'The image could not be retrieved, please contact Bynder Support');
-			if (typeof $image !== 'undefined') {
-				if (typeof $image.attr('src') !== 'undefined') {
-					$image.attr('src', retryToLoadImage($image));
-				}
-			}
-		});
-	});
-
-	function retryToLoadImage($img) {
-		var $newImg = $('<img>');
-		var $src = ($img.attr('src')) || '';
-		$newImg.attr('src', $src);
-		$newImg.one('error', function() {
-			window.setTimeout(function(){
-				if (retries > 0) {
-					retries--;
-					retryToLoadImage($newImg);
-				}
-			}, 1000);
-		});
-
-		$newImg.one('load', function() {
-			return $newImg.attr('src');
-		});
-	}
-})(jQuery);
+/* Try to reload the image with a 3 second interval.
+Do this by replacing to image source. */
+jQuery(document).ready(function() {
+    jQuery('.field-items .field-item img').error(function() {
+        $img = jQuery(this);
+        setTimeout(function() {
+            $img.attr('src', $img.attr('src'));
+        }, 3000);
+    });
+});

fbcc63b534fecf3f2166ff69cc29aa90177a2b8f
Thomas Moors <thomas.moors@getbynder.com>
2016-02-29 17:45:54 +0100

    3 times retry and better style

--- js/image.js
+++ js/image.js
@@ -1,5 +1,5 @@
-/* Try to reload the image with a 3 second interval.
-Do this by replacing to image source. */
+/* Try to reload the image with a 3 second interval, max 3 times.
+Do this by replacing the image source. */
 jQuery(document).ready(function() {
     $retries = 3;
     jQuery('.field-items .field-item img').error(function() {
@@ -8,8 +8,9 @@ jQuery(document).ready(function() {
             $img.attr('src', $img.attr('src'));
         }, 3000);
         $retries--;
-    if(retries < 0){
-        $img.unbind('error');
-    }
+
+        if($retries <= 0) {
+            $img.unbind('error');
+        }
     });
 });

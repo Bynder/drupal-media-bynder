bfa4bc843ae9bdfdaf08941bf30edc8f0a2ded96
Thomas Moors <thomas.moors@getbynder.com>
2016-02-29 17:28:20 +0100

    stop retry

--- js/image.js
+++ js/image.js
@@ -1,10 +1,15 @@
 /* Try to reload the image with a 3 second interval.
 Do this by replacing to image source. */
 jQuery(document).ready(function() {
+    $retries = 3;
     jQuery('.field-items .field-item img').error(function() {
         $img = jQuery(this);
         setTimeout(function() {
             $img.attr('src', $img.attr('src'));
         }, 3000);
+        $retries--;
+    if(retries < 0){
+        jQuery('.field-items .field-item img').unbind('error');
+    }
     });
 });

626365042d23feb80a9a3cd653d90c1fb64e8ea7
Thomas Moors <thomas.moors@getbynder.com>
2016-03-03 09:18:30 +0000

    preview selector

--- js/image.js
+++ js/image.js
@@ -1,9 +1,23 @@
 /* Try to reload the image with a 3 second interval, max 3 times.
 Do this by replacing the image source. */
 jQuery(document).ready(function() {
-    $retries = 3;
-    jQuery('.field-items .field-item img, div > div > div > div > p > img').error(function() {
+    $retries = 5;
+    jQuery('.field-items .field-item img').error(function() {
         $img = jQuery(this);
+        reloadImage($img);
+    });
+
+    jQuery("iframe").contents().find('div.preview img').error(function() {
+        $img = jQuery(this);
+        reloadImage($img);
+    });
+
+    jQuery("iframe").contents().find('iframe').contents().find('body[contenteditable] img').error(function() {
+        $img = jQuery(this);
+        reloadImage($img);
+    });
+
+    function reloadImage($img) {
         setTimeout(function() {
             $img.attr('src', $img.attr('src'));
         }, 3000);
@@ -12,5 +26,5 @@ jQuery(document).ready(function() {
         if($retries <= 0) {
             $img.unbind('error');
         }
-    });
+    }
 });
